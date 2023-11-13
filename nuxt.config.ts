170c4d178299fc86a3601e8bb90b1548abeb3431
José Puma <josepuma@sayrin.cl>
2023-11-13 14:47:13 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from '@nuxt/kit'
+const { resolve } = createResolver(import.meta.url)
 import svgLoader from 'vite-svg-loader'
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['@/assets/css/global.css'],
+  css: [
+    resolve('./assets/css/global.css')
+  ],
   modules: ['@nuxtjs/tailwindcss','@hypernym/nuxt-gsap', '@vueuse/nuxt', 'nuxt-icon'],
   gsap: {
     extraPlugins: {
